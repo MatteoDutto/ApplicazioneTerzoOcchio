7ce16fe863bac39d3a7cb2f5a16f0af6ae588442
Matteo Dutto <38939267+MatteoDutto@users.noreply.github.com>
2019-06-01 16:16:56 +0200

    Update MainActivity.java

--- qrReader/app/src/main/java/com/google/android/gms/samples/vision/barcodereader/MainActivity.java
+++ qrReader/app/src/main/java/com/google/android/gms/samples/vision/barcodereader/MainActivity.java
@@ -125,7 +125,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.register) {
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
 
